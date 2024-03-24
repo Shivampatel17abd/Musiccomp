73d47dd8fe9852968d9b26b0e48c9b4cfa8ecf85
Shivampatel17abd <shivamabdvk@gmail.com>
2024-03-24 13:43:07 +0530

    goo

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "../utils/cn";
 import Link from "next/link";
+import { act } from "react-dom/test-utils";
+import { actionAsyncStorage } from "next/dist/client/components/action-async-storage.external";
 
 
 function Navbar({ className }: { className?: string }) {
@@ -12,21 +14,24 @@ function Navbar({ className }: { className?: string }) {
     className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
             <Menu setActive={setActive}>
-                <Link href={"/"}>
-                <MenuItem setActive={setActive} active={active} item="Home">
-                    <div>
-                            home
-                    </div>
-                </MenuItem>
-                </Link>
 
-                <MenuItem setActive={setActive} active={active} item="our Courses">
-                <HoveredLink href="/hobby">Hobby</HoveredLink>
-                <HoveredLink href="/individual">Individual</HoveredLink>
-                <HoveredLink href="/team">Team</HoveredLink>
-                <HoveredLink href="/enterprise">Enterprise</HoveredLink>
+              <Link href={"/"}>
+                <MenuItem setActive={setActive} active={active} item="Home"></MenuItem>
+              </Link>
                 
-               </MenuItem>
+              <MenuItem setActive={setActive} active={active} item="Our courses" >
+                <div className="flex flex-col space-y-4">
+                  <HoveredLink href="/courses">All courses</HoveredLink>
+                  <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
+                  <HoveredLink href="/courses">Advance Composition</HoveredLink>
+                  <HoveredLink href="/courses">Song writing</HoveredLink>
+                  <HoveredLink href="/courses">Music Production</HoveredLink>
+                </div>
+              </MenuItem>
+
+              <Link href={'/'}>
+              <MenuItem setActive={setActive} active={active} item="Contact us"></MenuItem>
+              </Link>
             </Menu>
         </div>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 import Image from "next/image";
+import { HeroSection } from "./Components/ui/HeroSection";
+import FeaturedCourses from "./Components/FeaturedCourses";
 
 export default function Home() {
   return (
-      <h1 className="text-2xl text-center"> Shivam patel</h1>
+     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+               <h1 className="text-2xl text-center ">
+                          Shivam patel
+               </h1>
+
+                 <HeroSection/>
+                 <FeaturedCourses/>
+     </main>
   );
 }
